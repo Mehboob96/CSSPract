583553ae0a0a620c9750f113ae18eb33c0b7a6a6
Mehboob96 <smehboobs62@gmail.com>
2017-07-24 22:13:53 +0530

    Corrected MD5 Algo from home

--- Md5.java
+++ Md5.java
@@ -1,38 +1,41 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
-public class Md5 {
-
-	public static String getMdD5(String input){
-		try{
+public class MD5
+{
+	public static String getMD5(String input)
+	{
+		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			byte[] messageDigest = md.digest(input.getBytes());
-			System.out.println("");
+			byte[] MessageDigest = md.digest(input.getBytes());
+			System.out.println("Byte Representation" +MessageDigest);
 			
-			BigInteger number = new BigInteger(1,messageDigest);
-			System.out.println("number: " +number);
+			BigInteger number = new BigInteger(1, MessageDigest);
+			System.out.println("number" +number);
+			String hashtext = number.toString(16);
 			
-			String hashtext=number.toString(16);
-			
-			while(hashtext.length() < 32){
-				hashtext="0"+hashtext;
-			}
-			return hashtext;
+			while (hashtext.length() < 32) {
+			hashtext = "0" + hashtext;
 		}
-		catch(NoSuchAlgorithmException e){
-			throw new RuntimeException(e);
-		}
+		return hashtext;
 	}
-	
-	
-	public static void main(String[] args) throws {
-		// TODO Auto-generated method stub
-		
-		
-		
-
+	catch (NoSuchAlgorithmException e) {
+		throw new RuntimeException(e);
 	}
+}
 
+
+public static void main(String[] args) throws
+NoSuchAlgorithmException, IOException {
+	
+	System.out.println("Enter the string");
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String text1 = br.readLine();
+	System.out.println("Hash Value: "+getMD5("text1"));
+}
 }
